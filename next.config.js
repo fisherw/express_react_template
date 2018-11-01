217abfdd38114d873696351a7fcf36900053724f
汪江 <wangjiang@iaskdoc.com>
2018-11-01 17:35:29 +0800

    完善支持图片的引用及cdn上传

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
+// var path = require('path');
 const withPlugins = require('next-compose-plugins');
+const WebpackUploadPlugin = require('webpack-upload');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const withCSS = require('@zeit/next-css');
 const withLess = require('@zeit/next-less');
+// const withImages = require('next-images');
+const optimizedImages = require('next-optimized-images');
 
 
 const prod = process.env.NODE_ENV === 'production';
 
 const plugins = [
+    [optimizedImages, {
+        imagesFolder: 'imgs',
+        imagesPublicPath: prod ? '/official/_next/static/imgs/': '',
+        optimizeImagesInDev: false,
+        inlineImageLimit: 3072,
+    }],
     [withCSS, {
         cssLoaderOptions: {
             importLoaders: 2
@@ -18,16 +29,24 @@ const plugins = [
         //     // parser: false, 
         //     autoprefixer: true
         // }
+        lessLoaderOptions: {
+            javascriptEnabled: true,
+        },
     }],
 ];
+
+// fix: prevents error when .css files are required by node
+if (typeof require !== 'undefined') {
+    require.extensions['.less'] = (file) => {}
+}
 
 module.exports = withPlugins([...plugins],{
     useFileSystemPublicRoutes: false,
     distDir: 'build',
-    assetPrefix: prod? '//xxxx.com/rs/official': '',
+    assetPrefix: prod? '//cdn.xxx.cc.com/official/': '',
     pageExtensions: ['jsx', 'js'],
     generateBuildId: async () => {
-        return 'ym-official-build-id'
+        return 'bid2'
     },
     webpack: (config, options) => {
         const { isServer, buildId } = options
@@ -35,6 +54,35 @@ module.exports = withPlugins([...plugins],{
         // Fixes npm packages that depend on `fs` module
         config.node = {
             fs: 'empty'
+        }
+
+        // config.module.rules.push({
+        //     test: /\.(png|jpg|gif)/,
+        //     exclude: /node_modules/,
+        //     use: 'url-loader?limit=3072&name=[path][name].[hash].[ext]&useRelativePath=false&context=', // inline base64 URLs for <=3k images
+        // });
+        // config.module.rules.push({
+        //     test: /\.(txt|jpg|png|gif)$/,
+        //     exclude: /node_modules/,
+        //     use: [{
+        //         loader: 'file-loader',
+        //         options: {
+        //             context: '',
+        //             emitFile: true,
+        //             limit: 3072,
+        //             name: '[path][name].[hash].[ext]'
+        //         }
+        //     }]
+        // });
+
+        if (prod && !isServer) {
+            config.plugins.push(// 静态资源实现cdn上传
+                new WebpackUploadPlugin({
+                    receiver: 'http://receiver.yunmicloud.xin/receiver',
+                    to: '/mnt02/res/official/_next'
+                })
+            );
+            
         }
 
         return config;
